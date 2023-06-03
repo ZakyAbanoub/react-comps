ec536f2e6b07db7af532ea8518a5fa30ada7a534
Abanoub Zaky <zaky.abanoub@gmail.com>
2023-06-04 02:32:16 +0300

    add custom table - to do new refactor

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,23 +1,29 @@
-const Table = ({ data }) => {
-  const renderedRows = data.map((fruit) => {
+const Table = ({ data, config, keyFn }) => {
+  const renderedHeders = config.map((column) => {
     return (
-      <tr className="border-b" key={fruit.name}>
-        <td className="p-3">{fruit.name}</td>
-        <td className="p-3">
-          <div className={`p-3 m-2 ${fruit.color}`}></div>
+      <th key={column.label}>
+        <span>{column.label}</span>
+      </th>
+    );
+  });
+  const renderedRows = data.map((rowData) => {
+    const renderdCells = config.map((column) => {
+      return (
+        <td className="p-2" key={column.label}>
+          {column.render(rowData)}
         </td>
-        <td className="p-3">{fruit.score}</td>
+      );
+    });
+    return (
+      <tr className="border-b" key={keyFn(rowData)}>
+        {renderdCells}
       </tr>
     );
   });
   return (
     <table className="table-auto border-spacing-2">
       <thead>
-        <tr className="border-b-2">
-          <th>Fruit</th>
-          <th>Color</th>
-          <th>Score</th>
-        </tr>
+        <tr className="border-b-2">{renderedHeders}</tr>
       </thead>
       <tbody>{renderedRows}</tbody>
     </table>
